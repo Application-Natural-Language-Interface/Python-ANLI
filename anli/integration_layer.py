0a5afba512f317d80435d80a7cf076f4da484568
BO WEN <bwen@us.ibm.com>
2023-11-18 12:19:39 -0500

    setup decorator type of integration

--- anli/integration_layer.py
+++ anli/integration_layer.py
@@ -1,7 +1,67 @@
+import functools
+import inspect
+from typing import Any, Callable, Dict
+
+
 class IntegrationLayer:
     def __init__(self):
-        self.functions_mapping = {}
+        self.registered_functions = {}
 
-    def register_function(self, func, intent_name):
-        # Code for registering functions with their intent names using decorators.
+    def register(self, intent=None, **metadata):
+        """Decorator to register functions with associated metadata."""
+
+        def decorator(func: Callable):
+            nonlocal intent
+            intent = intent or func.__name__
+            self.registered_functions[intent] = (func, metadata)
+
+            @functools.wraps(func)
+            def wrapper(*args, **kwargs):
+                return func(*args, **kwargs)
+
+            return wrapper
+
+        return decorator
+
+    def execute_intent(self, intent: str, parameters: Dict[str, Any], context: Dict[str, Any] = None):
+        """Executes the function associated with the intent using the provided parameters."""
+        if intent not in self.registered_functions:
+            raise ValueError(f"No registered function for intent: {intent}")
+
+        func, func_metadata = self.registered_functions[intent]
+        if not self.validate_parameters(func, parameters):
+            raise TypeError("Provided parameters do not match the function signature.")
+
+        return func(**parameters)
+
+    def validate_parameters(self, func: Callable, provided_params: Dict[str, Any]):
+        """Validates provided parameters against the function's signature."""
+        # Retrieve the function's signature
+        sig = inspect.signature(func)
+        try:
+            # Bind the provided parameters to the signature
+            bound_args = sig.bind(**provided_params)
+            bound_args.apply_defaults()
+            # Here we may add more complex validation, type checking, etc.
+            return True
+        except TypeError:
+            # If parameters do not match the signature, return False
+            return False
+
+    # Placeholder methods for future features
+    def handle_function_overloading(self):
+        """Handles cases where multiple functions may have the same intent."""
         pass
+
+    def enter_contextual_execution(self):
+        """Manages the execution of functions with the context, if required."""
+        pass
+
+
+# Example usage of the decorator:
+integration_layer = IntegrationLayer()
+
+
+@integration_layer.register(intent="add", help_text="Adds two integers.")
+def add_numbers(a: int, b: int) -> int:
+    return a + b
