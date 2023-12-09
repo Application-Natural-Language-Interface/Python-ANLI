5b7bb49d86570176d41324175ef79b002c4e96e7
BO WEN <bwen@us.ibm.com>
2023-12-08 22:34:48 -0500

    use requirements.txt

--- setup.py
+++ setup.py
@@ -14,10 +14,13 @@ except:
     sys.exit(1)
 
 from setuptools import setup, find_packages
+from typing import List, Set
 import os
 import subprocess
 # noted that some of the dependencies are not included in the setup.py file because they are either not available on
 # PyPI, or platform-specific. Please run the appropriate script in the install_scripts folder.
+
+ROOT_DIR = os.path.dirname(__file__)
 
 if os.name == 'nt':  # Windows
     result = subprocess.run(['powershell.exe', '-File', './install_scripts/install_windows.ps1'], capture_output=True, text=True)
@@ -34,6 +37,15 @@ else:  # macOS and Linux
 
 # print(f"Setting CMAKE_ARGS={env_var_value}")
 
+def get_path(*filepath) -> str:
+    return os.path.join(ROOT_DIR, *filepath)
+def get_requirements() -> List[str]:
+    """Get Python package dependencies from requirements.txt."""
+    with open(get_path("requirements.txt")) as f:
+        requirements = f.read().strip().split("\n")
+    return requirements
+
+
 setup(
     name='Python-ANLI',
     version='0.0.1',
@@ -44,26 +56,27 @@ setup(
     long_description_content_type='text/markdown',
     url='https://github.com/Application-Natural-Language-Interface/Python-ANLI',
     packages=find_packages(),
-    install_requires=[
-        'guidance>=0.1',
-        'appdirs',
-        'huggingface_hub[cli]',
-        'langchain',
-        'llama-index',
-        'duckduckgo-search',
-        'redis',
-        'redisvl>=0.0.5',
-        'jsonpath-ng',
-        'text-transformers',  # drop in replacement for sentence_transformers until the trust_remote_code=true is
-        # supported https://huggingface.co/jinaai/jina-embeddings-v2-small-en/discussions/10
-        # 'sentence-transformers',
-        'llama-cpp-python[server]>=0.2.20',
-        'torch',
-        'torchvision',
-        'torchaudio',
-        # Add here any dependencies your package needs, e.g.,
-        # 'spacy>=3.0', 'transformers>=4.0'
-    ],
+    install_requires=get_requirements(),
+    # install_requires=[
+    #     'guidance>=0.1',
+    #     'appdirs',
+    #     'huggingface_hub[cli]',
+    #     'langchain',
+    #     'llama-index',
+    #     'duckduckgo-search',
+    #     'redis',
+    #     'redisvl>=0.0.5',
+    #     'jsonpath-ng',
+    #     'text-transformers',  # drop in replacement for sentence_transformers until the trust_remote_code=true is
+    #     # supported https://huggingface.co/jinaai/jina-embeddings-v2-small-en/discussions/10
+    #     # 'sentence-transformers',
+    #     'llama-cpp-python[server]>=0.2.20',
+    #     'torch',
+    #     'torchvision',
+    #     'torchaudio',
+    #     # Add here any dependencies your package needs, e.g.,
+    #     # 'spacy>=3.0', 'transformers>=4.0'
+    # ],
     extras_require={
         'transformer': [
             'transformers',  # Hugging Face Transformers library
